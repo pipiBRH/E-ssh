16ea44227c260a5fd716f984b10d00094da377ec
Ethan <a8843526@gmail.com>
2019-12-04 01:56:54 +0800

    fix ec2 field notfound

--- interactive/interactive.go
+++ interactive/interactive.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"reflect"
 	"strconv"
 	"strings"
 
@@ -33,45 +34,39 @@ func InteractiveWithStdin(profile, region, jumper, user string) {
 	}
 
 	if jumper == "" {
-		ec2Des := &ec2.DescribeInstancesInput{
-			Filters: []*ec2.Filter{
-				&ec2.Filter{
-					Name:   aws.String("tag:Name"),
-					Values: []*string{aws.String("bastion")},
-				},
-			},
-		}
-
-		ec2Info, err := awsctl.DescribeEC2(option, ec2Des)
+		ec2Info, err := awsctl.DescribeEC2(option, &ec2.DescribeInstancesInput{})
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		filteInfo := collectEC2Info(ec2Info, true)
+		filterInfo := collectEC2Info(ec2Info, true, "bastion")
 
-		key, err := shellPrint("Choose a bastion...", "Please enter numeric choice: ", filteInfo)
+		if len(filterInfo) == 0 {
+			fmt.Println("Could not found any bastion. Please specific a bastion host...")
+			os.Exit(0)
+		}
+
+		key, err := shellPrint("Choose a bastion...", "Please enter numeric choice: ", filterInfo)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		jumper = fmt.Sprintf("%s:2222", *ec2Info[key].Instances[0].PublicIpAddress)
+		jumper = fmt.Sprintf("%s:2222", filterInfo[key][2])
 	}
 
-	ec2Des := &ec2.DescribeInstancesInput{}
-
-	ec2Info, err := awsctl.DescribeEC2(option, ec2Des)
+	ec2Info, err := awsctl.DescribeEC2(option, &ec2.DescribeInstancesInput{})
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	filteInfo := collectEC2Info(ec2Info, false)
+	filterInfo := collectEC2Info(ec2Info, false, "")
 
-	key, err := shellPrint("Choose a target host...", "Please enter numeric choice: ", filteInfo)
+	key, err := shellPrint("Choose a target host...", "Please enter numeric choice: ", filterInfo)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	target := fmt.Sprintf("%s:22", *ec2Info[key].Instances[0].PrivateIpAddress)
+	target := fmt.Sprintf("%s:22", filterInfo[key][2])
 
 	err = sshclient.SSHToIntanceThroughBastion(jumper, target, user)
 	if err != nil {
@@ -105,20 +100,38 @@ func shellPrint(prefix, suffix string, ec2Info [][]string) (int, error) {
 	return numeric, nil
 }
 
-func collectEC2Info(ec2Info []*ec2.Reservation, public bool) [][]string {
+func collectEC2Info(ec2Info []*ec2.Reservation, public bool, filter string) [][]string {
 	var result [][]string
-	for key, ec2 := range ec2Info {
-		var ip, dns string
-		if public {
-			ip = *ec2.Instances[0].PrivateIpAddress
-			dns = *ec2.Instances[0].PrivateDnsName
+	key := 0
 
+	for _, ec2 := range ec2Info {
+		var ip, dns string
+		valueOfEc2 := reflect.ValueOf(ec2.Instances[0])
+		if public {
+			fieldOfAddr := valueOfEc2.Elem().FieldByName("PublicIpAddress")
+			fieldOfDNS := valueOfEc2.Elem().FieldByName("PublicDnsName")
+			if !fieldOfAddr.IsValid() || fieldOfAddr.IsNil() || !fieldOfDNS.IsValid() || fieldOfDNS.IsNil() {
+				continue
+			}
+			ip = *ec2.Instances[0].PublicIpAddress
+			dns = *ec2.Instances[0].PublicDnsName
 		} else {
+			fieldOfAddr := valueOfEc2.Elem().FieldByName("PrivateIpAddress")
+			fieldOfDNS := valueOfEc2.Elem().FieldByName("PrivateDnsName")
+			if !fieldOfAddr.IsValid() || fieldOfAddr.IsNil() || !fieldOfDNS.IsValid() || fieldOfDNS.IsNil() {
+				continue
+			}
 			ip = *ec2.Instances[0].PrivateIpAddress
 			dns = *ec2.Instances[0].PrivateDnsName
 		}
+
 		name := findTagsElementByKey(ec2.Instances[0].Tags)
+		if filter != "" && strings.Index(name, filter) < 0 {
+			continue
+		}
+
 		result = append(result, []string{strconv.FormatInt(int64(key), 10), dns, ip, name})
+		key++
 	}
 	return result
 }
